525ded68c16ce5748a566917bcaecfadd2700284
Motivationman_74 <120492095+Motivetionman@users.noreply.github.com>
2023-12-20 03:00:27 +0700

    Update lab10_3.cpp

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,45 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ifstream inputFile("score.txt");
+
+    if (!inputFile.is_open()) {
+        cout << "Error opening the file." << endl;
+        return 1;
+    }
+
+    
+    vector<double> data;
+    double value;
+    while (inputFile >> value) {
+        data.push_back(value);
+    }
+
+    inputFile.close();
+
+    
+    double sum = 0.0;
+    for (double num : data) {
+        sum += num;
+    }
+    double mean = sum / data.size();
+    double sumOfSquares = 0.0;
+    for (double num : data) {
+        sumOfSquares += pow(num - mean, 2);
+    }
+    double standardDeviation = sqrt(sumOfSquares / data.size());
+
+    
+    cout << "Number of data = " << data.size() << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << standardDeviation << endl;
+
+    return 0;
+}
