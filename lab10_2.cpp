cbc9bb27d1263ca39de0fba0b7c4b48861c95e0a
Motivationman_74 <120492095+Motivetionman@users.noreply.github.com>
2023-12-20 03:01:36 +0700

    Update lab10_2.cpp

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -8,10 +8,20 @@ int main (){
 	ofstream dest;
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
-	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	if (!source.is_open() || !dest.is_open()) {
+        cout << "Error opening files." << endl;
+        return 1; 
+    }
 
+    
+    string line;
+    dest << "-------------------- BOOM ---------------------" << endl;
+
+    while (getline(source, line)) {
+        dest << line << endl;
+    }
+
+    dest << "-------------------- HA!! ---------------------" << endl;
     source.close();
     dest.close();
 	return 0;
